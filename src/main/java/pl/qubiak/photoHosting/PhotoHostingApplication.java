7a42784294691fb6be48c42d9a60bcebd38c15f3
qubiak <kubiak1986@gmail.com>
2021-04-28 09:27:14 +0200

    init

---
+++ src/main/java/pl/qubiak/photoHosting/PhotoHostingApplication.java
@@ -0,0 +1,13 @@
+package pl.qubiak.photoHosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoHostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoHostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/qubiak/photoHosting/PhotoHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.qubiak.photoHosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoHostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
