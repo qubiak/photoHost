be0557892ff419873a359aecd91756e38cb4cd63
qubiak <kubiak1986@gmail.com>
2021-05-04 14:58:27 +0200

    some problem with token

--- src/main/java/pl/qubiak/photoHosting/Controller/TokenController.java
+++ src/main/java/pl/qubiak/photoHosting/Controller/TokenController.java
@@ -1,34 +1,38 @@
 package pl.qubiak.photoHosting.Controller;
 
 
+
+import com.vaadin.flow.component.UI;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.qubiak.photoHosting.Model.AppUser;
 import pl.qubiak.photoHosting.Model.Token;
 import pl.qubiak.photoHosting.Repo.AppUserRepo;
 import pl.qubiak.photoHosting.Repo.TokenRepo;
 
 @Controller
-public class TokenViev {
+public class TokenController {
 
     private AppUserRepo appUserRepo;
     private TokenRepo tokenRepo;
 
-    public TokenViev(AppUserRepo appUserRepo, TokenRepo tokenRepo) {
+    public TokenController(AppUserRepo appUserRepo, TokenRepo tokenRepo) {
         this.appUserRepo = appUserRepo;
         this.tokenRepo = tokenRepo;
     }
 
     @GetMapping("/token")
-    public String singup(String value) {
+    public String singup(@RequestParam String value) {
         Token byValue = tokenRepo.findByValue(value);
-        System.out.println(byValue.getAppUser());
         AppUser appUser = byValue.getAppUser();
         appUser.setEnabled(true);
         appUserRepo.save(appUser);
-        return "email confirmed, now pleas login";
+        return "login";
     }
 }
+
+
 
 
 
